4ef8c924a9fc1e53ebbb27d5cf853dd47b5dd82d
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-02 00:55:39 +0800

    query

--- src/resolvers/account.resolver.ts
+++ src/resolvers/account.resolver.ts
@@ -1,9 +1,13 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { AccountService } from '../account/account.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Resolver('Account')
 export class AccountResolver {
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private jwtService: JwtService,
+  ) {}
 
   @Mutation('signUp')
   async signUp(@Args('input') signUpInput: any) {
@@ -32,5 +36,28 @@ export class AccountResolver {
       token,
       user,
     };
+  }
+
+  @Query('me')
+  async me(@Context() context: any) {
+    const authHeader = context.req.headers.authorization;
+    if (!authHeader) {
+      throw new Error('Authorization header not found');
+    }
+
+    const [, token] = authHeader.split(' ');
+    const decodedToken = this.jwtService.decode(token) as any;
+
+    if (!decodedToken?.username) {
+      throw new Error('Invalid token');
+    }
+
+    const user = await this.accountService.retrieve({
+      username: decodedToken.username,
+    });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
   }
 }
