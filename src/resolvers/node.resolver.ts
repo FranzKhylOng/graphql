3c5505a8cf0dd3ad36450ff880fa5f3de9d79301
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-15 10:39:04 +0800

    feat(query) working product retrieval with node query

--- src/resolvers/node.resolver.ts
+++ src/resolvers/node.resolver.ts
@@ -4,7 +4,6 @@ import { ProductService } from '../product/product.service';
 import { User } from '../account/account.model';
 import { Product } from '../product/product.model';
 
-//didn't get to make it work for product in time
 @Resolver('NodeResult')
 export class NodeResolver {
   constructor(
@@ -12,19 +11,22 @@ export class NodeResolver {
     private readonly productService: ProductService,
   ) {}
 
-  @Query(() => User, { name: 'node' })
+  @Query(() => 'NodeResult', { name: 'node' })
   async getNode(
     @Args('id', { type: () => String }) id: string,
   ): Promise<User | Product> {
-    console.log('getNode resolver called with ID:', id);
-    const account = await this.accountService.retrieveById(id);
-    if (account) {
-      return account;
+    try {
+      const account = await this.accountService.retrieveById(id);
+      if (account) {
+        return account;
+      }
+    } catch (e) {
+      console.error(e.message);
     }
 
     const product = await this.productService.retrieve(id);
     if (product) {
-      return product as Product;
+      return product;
     }
 
     throw new Error('Entity not found for the provided ID.');
