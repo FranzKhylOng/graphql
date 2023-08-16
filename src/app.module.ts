d93f3accbe983b69ada8a8a5782e8a0c4ecb54eb
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-16 16:35:54 +0800

    refactor(app module) use configmodule and configservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,26 @@ import { AccountModule } from './account/account.module';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DateTimeResolver, EmailAddressResolver } from 'graphql-scalars';
-import { privateDirectiveTransformerFactory } from './libs/privatedirective';
+import { privateDirectiveTransformerFactory } from './libs/private-directive';
 import { BinaryScalar } from './scalars/binary.scalar';
 import { AccountResolver } from './resolvers/account.resolver';
 import { ProductResolver } from './resolvers/product.resolver';
 import { NodeResolver } from './resolvers/node.resolver';
 import { DataLoaderService } from './libs/data-loader.service';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nestjs'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        return {
+          uri: config.get('MONGODB_URI') || 'mongodb://localhost:27017',
+        };
+      },
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       useFactory: () => {
