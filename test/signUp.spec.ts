6a0cc98aa4eb3b45c75a43dabd8e91596e7f7d2d
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-22 13:03:05 +0800

    test(signUp): added test for existing user

--- test/signUp.spec.ts
+++ test/signUp.spec.ts
@@ -2,32 +2,32 @@ import { fixture } from './fixture';
 import { faker } from '@faker-js/faker';
 
 describe('signUp account', () => {
+  const userbody = {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  };
+
+  const mutation = `
+  mutation($input: SignUpInput!){
+    signUp(input: $input){
+      token
+    }
+  }
+`;
+
+  const variables = {
+    input: {
+      emailAddress: userbody.email,
+      password: userbody.password,
+      firstname: userbody.firstName,
+      lastname: userbody.lastName,
+    },
+  };
+
   test.concurrent('creates user', async () => {
     const { request, teardown } = await fixture();
-
-    const userbody = {
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-    };
-
-    const mutation = `
-      mutation($input: SignUpInput!){
-        signUp(input: $input){
-          token
-        }
-      }
-    `;
-
-    const variables = {
-      input: {
-        emailAddress: userbody.email,
-        password: userbody.password,
-        firstname: userbody.firstName,
-        lastname: userbody.lastName,
-      },
-    };
 
     const response = await request
       .post('/graphql')
@@ -39,6 +39,30 @@ describe('signUp account', () => {
 
     expect(response.body.errors).toBeUndefined();
     expect(response.body.data.signUp.token).toBeDefined();
+
+    await teardown();
+  });
+
+  test.concurrent('tests for existing user', async () => {
+    const { request, teardown } = await fixture();
+
+    await request
+      .post('/graphql')
+      .send({
+        query: mutation,
+        variables: variables,
+      })
+      .expect(200);
+
+    const secondResponse = await request
+      .post('/graphql')
+      .send({
+        query: mutation,
+        variables: variables,
+      })
+      .expect(200);
+
+    expect(secondResponse.body.errors[0].message).toBe('BAD_USER_INPUT');
 
     await teardown();
   });
