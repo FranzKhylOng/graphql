4f523cffd5abd7c9497b85781bcdfdb1b3073d29
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-22 21:59:09 +0800

    test(deleteProduct) check if delete product is true and invalid token test

--- test/deleteProduct.spec.ts
+++ test/deleteProduct.spec.ts
@@ -1,3 +1,4 @@
+import exp from 'constants';
 import { fixture, createProductAndGetId, loginAndGetToken } from './fixture';
 
 describe('deleteProduct', () => {
@@ -22,7 +23,30 @@ describe('deleteProduct', () => {
       })
       .set('Authorization', `Bearer ${token}`)
       .expect(200);
+    expect(response.body.data.deleteProduct).toBe(true);
     expect(response.body.errors).toBeUndefined();
+    await teardown();
+  });
+
+  test.concurrent('invalid token', async () => {
+    const { request, teardown } = await fixture();
+    const productid = await createProductAndGetId(request);
+    const variables = {
+      input: {
+        id: productid,
+      },
+    };
+
+    const token = 'I_am_a_wrong_token';
+    const response = await request
+      .post('/graphql')
+      .send({
+        query: deleteMutation,
+        variables: variables,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+    expect(response.body.errors[0].message).toBe('Invalid token');
     await teardown();
   });
 });
