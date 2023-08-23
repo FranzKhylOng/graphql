4c8e786bd1a9ce5413d293aa5a1878a6cc01818f
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-23 22:11:22 +0800

    refactor(node resolver): node instead of noderesult

--- src/resolvers/node.resolver.ts
+++ src/resolvers/node.resolver.ts
@@ -1,32 +1,15 @@
-import { Resolver, Query, Args, ResolveField } from '@nestjs/graphql';
+import { Resolver, ResolveField } from '@nestjs/graphql';
 import { AccountService } from '../account/account.service';
 import { ProductService } from '../product/product.service';
 import { User } from '../account/account.model';
 import { Product } from '../product/product.model';
 
-@Resolver('NodeResult')
+@Resolver('Node')
 export class NodeResolver {
   constructor(
     private readonly accountService: AccountService,
     private readonly productService: ProductService,
   ) {}
-
-  @Query(() => 'NodeResult', { name: 'node' })
-  async getNode(
-    @Args('id', { type: () => String }) id: string,
-  ): Promise<User | Product> {
-    const account = await this.accountService.retrieveById(id);
-    if (account) {
-      return account;
-    }
-
-    const product = await this.productService.retrieveById(id);
-    if (product) {
-      return product;
-    }
-
-    throw new Error('Entity not found for the provided ID.');
-  }
 
   @ResolveField()
   async __resolveType(obj: User | Product) {
