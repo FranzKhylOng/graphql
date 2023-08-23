101a9dd50112988d1d7548da021c4a9fdea546d2
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-23 09:29:22 +0800

    test(product query): after argument test

--- test/queryProduct.spec.ts
+++ test/queryProduct.spec.ts
@@ -1,7 +1,6 @@
 import { fixture, createProductAndGetId } from './fixture';
 describe('queryProducts', () => {
-  test.concurrent('gets product', async () => {
-    const productQuery = `query {
+  const productQuery = `query {
         products(first: 10) {
           edges {
             cursor
@@ -18,6 +17,7 @@ describe('queryProducts', () => {
         }
       }
       `;
+  test.concurrent('gets product', async () => {
     const { request, teardown } = await fixture();
 
     const productCount = 10;
@@ -34,6 +34,56 @@ describe('queryProducts', () => {
     expect(response.body.data.products).toHaveProperty('edges');
     expect(response.body.data.products).toHaveProperty('pageInfo');
     expect(response.body.errors).toBeUndefined();
+    await teardown();
+  });
+
+  test.concurrent('use after filter', async () => {
+    const { request, teardown } = await fixture();
+
+    const productCount = 10;
+    for (let i = 0; i < productCount; i++) {
+      await createProductAndGetId(request);
+    }
+
+    const response = await request
+      .post('/graphql')
+      .send({
+        query: productQuery,
+      })
+      .expect(200);
+    expect(response.body.data.products).toHaveProperty('edges');
+    expect(response.body.data.products).toHaveProperty('pageInfo');
+    expect(response.body.errors).toBeUndefined();
+
+    const cursor = response.body.data.products.edges[0].cursor;
+
+    const afterQuery = `query {
+        products(first: 10, after: "${cursor}") {
+          edges {
+            cursor
+            node {
+              name
+              id
+              description
+            }
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+      `;
+
+    const newresponse = await request
+      .post('/graphql')
+      .send({
+        query: afterQuery,
+      })
+      .expect(200);
+    expect(newresponse.body.data.products).toHaveProperty('edges');
+    expect(newresponse.body.data.products).toHaveProperty('pageInfo');
+    expect(newresponse.body.errors).toBeUndefined();
     await teardown();
   });
 });
