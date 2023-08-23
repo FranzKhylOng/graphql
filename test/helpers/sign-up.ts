52a6a27ed0042d3d493efe94b9b3fda9b7d99f46
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-23 17:11:00 +0800

    refactor: sign-up function

--- test/helpers/sign-up.ts
+++ test/helpers/sign-up.ts
@@ -14,16 +14,20 @@ export async function signUp(
   request: supertest.SuperTest<supertest.Test>,
   params?: Partial<User>,
 ) {
+  const user = {
+    emailAddress: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    ...(params || {}),
+  };
+
   await request.post('/graphql').send({
     query: signUpMutation,
     variables: {
-      input: {
-        emailAddress: faker.internet.email(),
-        password: faker.internet.password(),
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        ...(params || {}),
-      },
+      input: user,
     },
   });
+
+  return user;
 }
