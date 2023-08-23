284aba7b43f5f7b089ea8153fbc630ef0ac4965d
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-24 01:09:12 +0800

    refactor(test): generate product details function arguments

--- test/helpers/generate-product-details.ts
+++ test/helpers/generate-product-details.ts
@@ -3,7 +3,8 @@ import { Product } from '../../src/libs/types';
 
 export function generateProductDetails(
   params?: Partial<Omit<Product, 'owner'>>,
-): Partial<Omit<Product, 'owner'>> {
+): Partial<Omit<Product, 'owner' | 'name' | 'description'>> &
+  Pick<Product, 'name' | 'description'> {
   return {
     name: faker.commerce.product(),
     description: faker.commerce.productDescription(),
