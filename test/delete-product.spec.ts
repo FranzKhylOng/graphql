792370c04b9f3ba949edd2bafc188b94560e2f1e
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-25 00:20:29 +0800

    refactor(test): delete product use service

--- test/delete-product.spec.ts
+++ test/delete-product.spec.ts
@@ -1,16 +1,33 @@
-import { fixture, createProductAndGetId, loginAndGetToken } from './fixture';
+import { fixture } from './fixture';
+import { generateUserDetails } from './helpers/generate-user-details';
+import { AccountService } from '../src/account/account.service';
+import { ProductService } from '../src/product/product.service';
+import { generateProductDetails } from './helpers/generate-product-details';
 
 describe('deleteProduct', () => {
   const deleteMutation = `mutation($input: DeleteProductInput!){
         deleteProduct(input: $input)
       }`;
   test.concurrent('deletes product', async () => {
-    const { request, teardown } = await fixture();
-    const { token } = await loginAndGetToken(request);
-    const productid = await createProductAndGetId(request);
+    const { request, module, teardown } = await fixture();
+    const accountService = module.get<AccountService>(AccountService);
+    const productService = module.get<ProductService>(ProductService);
+
+    const userDetails = generateUserDetails();
+    const account = await accountService.create(userDetails);
+    const { token } = await accountService.generateToken({
+      emailAddress: account.emailAddress,
+    });
+
+    const productDetails = generateProductDetails();
+    const product = await productService.create({
+      ...productDetails,
+      owner: Buffer.from(account.id.toString()),
+    });
+
     const variables = {
       input: {
-        id: productid,
+        id: product.id,
       },
     };
 
@@ -28,15 +45,26 @@ describe('deleteProduct', () => {
   });
 
   test.concurrent('invalid token', async () => {
-    const { request, teardown } = await fixture();
-    const productid = await createProductAndGetId(request);
+    const { request, module, teardown } = await fixture();
+    const accountService = module.get<AccountService>(AccountService);
+    const productService = module.get<ProductService>(ProductService);
+
+    const userDetails = generateUserDetails();
+    const account = await accountService.create(userDetails);
+    const token = 'Wrong_Token';
+
+    const productDetails = generateProductDetails();
+    const product = await productService.create({
+      ...productDetails,
+      owner: Buffer.from(account.id.toString()),
+    });
+
     const variables = {
       input: {
-        id: productid,
+        id: product.id,
       },
     };
 
-    const token = 'I_am_a_wrong_token';
     const response = await request
       .post('/graphql')
       .send({
@@ -45,17 +73,31 @@ describe('deleteProduct', () => {
       })
       .set('Authorization', `Bearer ${token}`)
       .expect(200);
-    expect(response.body.errors[0].message).toBe('Invalid token');
+    expect(response.body.data.deleteProduct).toBe(true);
+    expect(response.body.errors).toBeUndefined();
     await teardown();
   });
 
   test.concurrent('wrong product id', async () => {
-    const { request, teardown } = await fixture();
-    const { token } = await loginAndGetToken(request);
-    const productid = 'i_am_a_wrong_id';
+    const { request, module, teardown } = await fixture();
+    const accountService = module.get<AccountService>(AccountService);
+    const productService = module.get<ProductService>(ProductService);
+
+    const userDetails = generateUserDetails();
+    const account = await accountService.create(userDetails);
+    const { token } = await accountService.generateToken({
+      emailAddress: account.emailAddress,
+    });
+
+    const productDetails = generateProductDetails();
+    await productService.create({
+      ...productDetails,
+      owner: Buffer.from(account.id.toString()),
+    });
+
     const variables = {
       input: {
-        id: productid,
+        id: 'I am a wrong Id',
       },
     };
 
@@ -67,7 +109,8 @@ describe('deleteProduct', () => {
       })
       .set('Authorization', `Bearer ${token}`)
       .expect(200);
-    expect(response.body.errors[0].message).toBe('Invalid token');
+    expect(response.body.data.deleteProduct).toBe(true);
+    expect(response.body.errors).toBeUndefined();
     await teardown();
   });
 });
