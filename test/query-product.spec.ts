0b6e41aba98719f0d2c215c644a3507fea4ee820
FranzKhylOng <franzkhylm.ong@gmail.com>
2023-08-29 10:28:13 +0800

    test(product): after product query test

--- test/query-product.spec.ts
+++ test/query-product.spec.ts
@@ -49,4 +49,60 @@ describe('queryProducts', () => {
     expect(response.body.errors).toBeUndefined();
     await teardown();
   });
+
+  test.concurrent('after', async () => {
+    const { request, module, teardown } = await fixture();
+    const accountService = module.get<AccountService>(AccountService);
+    const productService = module.get<ProductService>(ProductService);
+
+    const productCount = 10;
+    for (let i = 0; i < productCount; i++) {
+      const userDetails = generateUserDetails();
+      const account = await accountService.create(userDetails);
+
+      const productDetails = generateProductDetails();
+      await productService.create({
+        ...productDetails,
+        owner: account.id.toString(),
+      });
+    }
+
+    const response = await request
+      .post('/graphql')
+      .send({
+        query: productQuery,
+      })
+      .expect(200);
+    const cursor = response.body.data.products.edges[0].cursor;
+
+    const newQuery = `query {
+      products(first: 10, after: "${cursor}") {
+        edges {
+          cursor
+          node {
+            name
+            id
+            description
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+    `;
+
+    const newresponse = await request
+      .post('/graphql')
+      .send({
+        query: newQuery,
+      })
+      .expect(200);
+
+    expect(newresponse.body.data.products).toHaveProperty('edges');
+    expect(newresponse.body.data.products).toHaveProperty('pageInfo');
+    expect(newresponse.body.errors).toBeUndefined();
+    await teardown();
+  });
 });
